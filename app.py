300d87ee608915b07c500efa63422cb36697c243
Hitler <murilogabrielhohmann@gmail.com>
2026-07-20 18:19:07 -0300

    Cu

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Rota principal (Tela de Login)
 @app.route("/")
-def index():
-    return render_template("index.html")
+def login():
+    return render_template("login.html")
+
+# Rota quando clicar em "Login"
+@app.route("/home")
+def home():
+    return render_template("home.html") # Lembre-se de criar esse HTML em templates/
+
+# Rota quando clicar em "Cadastrar"
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html") # Lembre-se de criar esse HTML em templates/
 
 if __name__ == "__main__":
     app.run(debug=True)
